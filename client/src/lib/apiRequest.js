32e4ec0057818a4f4c87d69f75848d72bdff6bc0
venom76568 <hagrawal560@gmail.com>
2025-11-12 17:46:42 +0530

    backend url updated

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://mern-project-rental-system.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-  }
+  },
 });
 
 export default apiRequest;
