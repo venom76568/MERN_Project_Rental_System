3076dcb80252e25660bf51eb0c3bb9a402d35cbb
venom76568 <hagrawal560@gmail.com>
2025-11-12 18:04:54 +0530

    backend cors

--- api/app.js
+++ api/app.js
@@ -10,7 +10,8 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
